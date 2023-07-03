f97bc6779ad3cca319d0320f351effb2f6976932
Marc GLOAGUEN <marc.gloaguen@gmail.com>
2023-07-03 17:06:25 +0200

    03 Django

--- NutriFood/settings.py
+++ NutriFood/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'NutriFood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'nutrifood',
+    'USER': 'Utilisateur',
+    'PASSWORD': '1234',
+    'HOST': 'localhost',
+    'PORT': '5432',
+}
 }
 
 
