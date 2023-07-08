edcf772e09083f09af6a726687d46c1b5926ac3a
Marc GLOAGUEN <marc.gloaguen@gmail.com>
2023-07-08 09:54:25 +0200

    View ok
    next step. : configurer ce qu'il faut afficher

--- NutriFood/urls.py
+++ NutriFood/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('search/', include('search.urls')),
     path('admin/', admin.site.urls),
+
 ]
--- search/views.py
+++ search/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Produit
 
 def index(request):
     return HttpResponse("Hey ! tu es sur l'index de l'application search du projet NutriFood.")
+
+def id(request, id):
+    object = Produit.objects.get(id=id)
+    context = {'brands': object.brands}
+
+    return render(request, 'search/id_infos.html', context)
