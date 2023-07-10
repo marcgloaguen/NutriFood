d2174723a15dc96e86ae40352e51a63326e5f863
Marc GLOAGUEN <marc.gloaguen@gmail.com>
2023-07-10 12:14:34 +0200

    modification bdd

--- search/management/commands/maj_bdd_opfofa.py
+++ search/management/commands/maj_bdd_opfofa.py
@@ -9,13 +9,12 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('Categories', nargs='+', type=str)
-        parser.add_argument('Maximum de produits', type=int, default=100)
 
     def handle(self, *args, **options):
         categories = options['Categories']
-        max_products = options['Maximum de produits']
+
         for categorie in categories:
-            url = f"https://world.openfoodfacts.org/category/{categorie}/{max_products}.json"
+            url = f"https://world.openfoodfacts.org/category/{categorie}/100.json"
             response = requests.get(url)
             if response.status_code == 200:
                 products = json.loads(response.text)['products']
@@ -24,12 +23,18 @@ class Command(BaseCommand):
                     brands = product.get('brands')
                     nutrition_grade_fr = product.get('nutrition_grade_fr')
                     countries = product.get('countries')
+                    product_name_fr = product.get('product_name_fr')
+                    product_name = product.get('product_name')
+                    pnns_groups_1 = product.get('pnns_groups_1')
 
                     product_obj = Produit(
                         id=_id,
                         brands=brands,
                         nutrition_grade_fr=nutrition_grade_fr,
                         countries=countries,
-                        tag=categories[0]
+                        product_name_fr=product_name_fr,
+                        product_name=product_name,
+                        pnns_groups_1=pnns_groups_1
+
                     )
                     product_obj.save()
--- search/models.py
+++ search/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class Produit(models.Model):
     id = models.CharField(primary_key=True)
-    tag = models.CharField(null=True)
     brands = models.TextField(null=True)
     nutrition_grade_fr = models.CharField(null=True)
     countries = models.CharField(null=True)
+    product_name_fr = models.CharField(null=True)
+    product_name = models.CharField(null=True)
+    pnns_groups_1 = models.CharField(null=True)
 
